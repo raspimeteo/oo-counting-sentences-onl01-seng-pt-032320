7860103d3ee57cb7638d23dc0f1950ec280e510c
John Zonneveld <jjczon@gmail.com>
2020-04-02 15:43:03 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.end_with?('.')
   end
 
   def question?
-
+   self.end_with?('?')
   end
 
   def exclamation?
-
+   self.end_with?('!')
   end
 
   def count_sentences
-
+  i = 0
+    a = self.split(/[!+.?]/) ## breaks on !, ? and . into array of strings
+    a.each do |str|
+      if str !=  ''
+        i = i + 1  # count the non-empty strings
+      end
+    end
+    return i
   end
 end
