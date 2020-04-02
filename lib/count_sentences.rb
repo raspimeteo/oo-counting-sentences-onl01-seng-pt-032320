c846b37d8f876f5e6525b0e0af42df4375ed2838
John Zonneveld <jjczon@gmail.com>
2020-04-02 15:42:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,14 +16,12 @@ class String
 
   def count_sentences
   i = 0
-    a = self.split(/[!+.?]/)
- ## 
-  a.each do |str|
-    if str !=  ''
-     ## binding.pry
-      i = i + 1
+    a = self.split(/[!+.?]/) ## breaks on !, ? and . into array of strings
+    a.each do |str|
+      if str !=  ''
+        i = i + 1  # count the non-empty strings
+      end
     end
-  end
     return i
   end
 end
