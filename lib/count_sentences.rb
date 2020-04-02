5ec20ddc3c4689c630db3dea39d3365f960f1a4f
John Zonneveld <jjczon@gmail.com>
2020-04-02 15:12:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.end_with?('.')
   end
 
   def question?
-
+   self.end_with?('?')
   end
 
   def exclamation?
-
+   self.end_with?('!')
   end
 
   def count_sentences
-
+    self
+    a = self.split(/[!.?]/)
+  binding.pry
+    return a.length
   end
 end
